002d5e0599deefb9eac3b4bc15feb87e98a95336
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-09-05 15:17:24 +0100

    fix:tailwind-grid

--- src/app/components/produit-card.tsx
+++ src/app/components/produit-card.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-export default function ProduitCard() {
+export type ShoppingCardProps = {
+  pourcentage: number;
+  imageUrl: string;
+  name: string;
+  oldPrice: number;
+  newPrice: number;
+  color?: string;
+};
+
+export default function ProduitCard(
+  { pourcentage,
+    imageUrl,
+    name,
+    oldPrice,
+    newPrice,
+    color
+  }: ShoppingCardProps
+) {
+
   return (
-    <div>
-      
+    <div className='border border-gray-200 p-4 space-y-4 bg-gray-100 w-[350px]'>
+      {/* Soldes & Pourcentage */}
+      <div className='flex justify-between items-center'>
+        <div className="flex justify-center items-center bg-black text-sm text-white size-12 rounded-full">
+          <p>soldes</p>
+        </div>
+        <p className="p-1 h-fit bg-red-600 text-white">{pourcentage}%</p>
+      </div>
+      {/* Image */}
+      <Image
+        src={imageUrl}
+        alt="Placeholder"
+        width={500}
+        height={500}
+        className="w-full h-100 object-contain"
+      />
+      {/* Description */}
+      <div className='flex justify-between items-center space-y-2'>
+        <div className='flex-col items-center'>
+          <p className='text-lg font-light uppercase'>{name}</p>
+          <div className='flex gap-4 items-center '>
+            <p className='text-lg font-light line-through text-gray-500'>{oldPrice} DH</p>
+            <p className='text-lg text-red-600'>{newPrice} DH</p>
+          </div>
+        </div>
+        {/* Couleurs */}
+        <div className={`size-6 ${color} rounded-full ms-auto mt-auto`} >
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/design/grid/page.tsx
+++ src/app/design/grid/page.tsx
@@ -1,11 +1,78 @@
-import { ChevronDown, Handbag, Heart, Mail, Search, UserRound } from 'lucide-react'
-import React from 'react'
+import { ChevronDown, Handbag, Heart, Mail, Search, UserRound } from 'lucide-react';
+import React from 'react';
+import ProduitCard from '@/app/components/produit-card';
 
+const products = [
+    {
+        pourcentage: 74,
+        imageUrl: "/images/grid-image/chemise.jpg",
+        name: 'Chemise Portefeuille',
+        oldPrice: 120,
+        newPrice: 90,
+        color: 'bg-white'
+    },
+    {
+        pourcentage: 50,
+        imageUrl: "/images/grid-image/menteau2.jpg",
+        name:'Manteau Classique',
+        oldPrice: 200,
+        newPrice: 100,
+        color: 'bg-amber-900'
+    },
+    {
+        pourcentage: 30,
+        imageUrl: "/images/grid-image/menteau1.jpg",
+        name: 'Manteau Long',
+        oldPrice: 180,
+        newPrice: 126,
+        color:'bg-black'
+    },
+    {
+        pourcentage: 65,
+        imageUrl: "/images/grid-image/jean1.jpg",
+        name: 'Jean Taille Haute',
+        oldPrice: 150,
+        newPrice: 75,
+        color: 'bg-blue-950'
+    },
+    {
+        pourcentage: 24,
+        imageUrl: "/images/grid-image/jean2.jpg",
+        name: 'ean blue',
+        oldPrice: 130,
+        newPrice: 98,
+        color: 'bg-blue-950'
+    },
+    {
+        pourcentage: 45,
+        imageUrl: "/images/grid-image/jupe2.jpg",
+        name:'Jupe longue',
+        oldPrice: 150,
+        newPrice:82,
+        color: 'bg-blue-500'
+    },
+    {
+        pourcentage: 15,
+        imageUrl: "/images/grid-image/robe1.jpg",
+        name:'Robe patineuse',
+        oldPrice: 80,
+        newPrice: 68,
+        color: 'bg-black'
+    },
+    {
+        pourcentage: 38,
+        imageUrl: "/images/grid-image/robe2.jpg",
+        name: 'Robe Longue noire',
+        oldPrice: 220,
+        newPrice: 136,
+        color: 'bg-black'
+    },
+]
 export default function page() {
     return (
-        <div className='px-10 space-y-4'>
+        <div className='relative px-10 space-y-4'>
             {/* Header */}
-            <div className=''>
+            <div className='fixed top-0 left-0 right-0 bg-white px-10 z-50 shadow-sm'>
                 <div className='flex justify-between items-center text-sm font-light py-2'>
                     <p>Langue:Français</p>
                     <p className='text-md font-normal'>RETOUR GRATUIT POUR TOUTE COMMANDE</p>
@@ -29,25 +96,37 @@ export default function page() {
                         <Handbag size={18} />
                     </div>
                 </div>
-                <div className='border-1 border-gray-100' />
             </div>
             {/* Sou menu */}
             <div className=''>
-                <p className='flex items-start px-4 mt-4'>Accueil / E-shop / Nouveautés
-                </p>
-                <p className='text-center text-6xl uppercase mt-5'>Vêtements
-                </p>
-                <div className='flex justify-between items-center mt-10'>
+                <p className='flex items-start px-4 mt-30'>Accueil / E-shop / Nouveautés</p>
+                <p className='text-center text-6xl uppercase mt-5'>Vêtements</p>
+                <div className='flex justify-between items-center mt-15'>
                     <span className='text-sm font-bold uppercase'>1818 Produits affichés
                     </span>
                     <div className='flex gap-6 items-center text-sm'>
-                        <p className='flex items-center'> Catégorie <ChevronDown size={18} fill='black'/> </p>
-                        <p className='flex items-center'> Taille <ChevronDown size={18} fill='black'/> </p>
-                        <p className='flex items-center'> Couleur <ChevronDown size={18} fill='black'/> </p>
-                        <p className='flex items-center'> Catégorie <ChevronDown size={18} fill='black'/> </p>
-                        <p className='flex items-center'>Trier par: <span className='font-bold pl-2'> Position</span> <ChevronDown size={18} fill='black'/></p>
+                        <p className='flex items-center'> Catégorie <ChevronDown size={18} fill='black' /> </p>
+                        <p className='flex items-center'> Taille <ChevronDown size={18} fill='black' /> </p>
+                        <p className='flex items-center'> Couleur <ChevronDown size={18} fill='black' /> </p>
+                        <p className='flex items-center'> Catégorie <ChevronDown size={18} fill='black' /> </p>
+                        <p className='flex items-center'>Trier par: <span className='font-bold pl-2'> Position</span> <ChevronDown size={18} fill='black' /></p>
                     </div>
                 </div>
+            </div>
+            {/* Grid */}
+            <div className='grid grid-cols-3 gap-4 my-10'>
+                {products.map((item, index) => (
+                    <div key={index}>
+                        <ProduitCard
+                            pourcentage={item.pourcentage}
+                            imageUrl={item.imageUrl}
+                            name={item.name}
+                            oldPrice={item.oldPrice}
+                            newPrice={item.newPrice}
+                            color={item.color}
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     )
