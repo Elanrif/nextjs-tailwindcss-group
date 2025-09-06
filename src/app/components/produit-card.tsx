5c921ad479a1b5f984602e53ce147e290cd2fdda
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-09-06 16:06:46 +0100

    fix:tailwind-grid

--- src/app/components/produit-card.tsx
+++ src/app/components/produit-card.tsx
@@ -21,33 +21,36 @@ export default function ProduitCard(
 ) {
 
   return (
-    <div className='border border-gray-200 p-4 space-y-4 bg-gray-100 w-[350px]'>
+    <div className='p-4 space-y-4'>
       {/* Soldes & Pourcentage */}
-      <div className='flex justify-between items-center'>
-        <div className="flex justify-center items-center bg-black text-sm text-white size-12 rounded-full">
-          <p>soldes</p>
+      <div className='justify-between items-center bg-gray-100 p-2 mb-2 w-[350px] '>
+        <div className='flex justify-between items-center px-2 mb-2'>
+          <div className="flex justify-center items-center bg-black text-sm text-white size-12 rounded-full">
+            <p>soldes</p>
+          </div>
+          <p className="p-1 h-fit bg-red-600 text-white">{pourcentage}%</p>
         </div>
-        <p className="p-1 h-fit bg-red-600 text-white">{pourcentage}%</p>
+
+        {/* Image */}
+        <Image
+          src={imageUrl}
+          alt="Placeholder"
+          width={500}
+          height={500}
+          className="w-full h-100 object-contain"
+        />
       </div>
-      {/* Image */}
-      <Image
-        src={imageUrl}
-        alt="Placeholder"
-        width={500}
-        height={500}
-        className="w-full h-100 object-contain"
-      />
       {/* Description */}
-      <div className='flex justify-between items-center space-y-2'>
+      <div className='flex border items-center space-y-2'>
         <div className='flex-col items-center'>
           <p className='text-lg font-light uppercase'>{name}</p>
-          <div className='flex gap-4 items-center '>
-            <p className='text-lg font-light line-through text-gray-500'>{oldPrice} DH</p>
-            <p className='text-lg text-red-600'>{newPrice} DH</p>
-          </div>
-        </div>
+          <div className='flex gap-4 items-center justify-between'>
+            {/* Prix */}
+            <p className='text-lg font-light line-through text-gray-500'>{oldPrice} MAD</p>
+            <p className='text-lg text-red-600'>{newPrice} MAD</p>
         {/* Couleurs */}
-        <div className={`size-6 ${color} rounded-full ms-auto mt-auto`} >
+        <div className={`size-6 ${color} rounded-full`} />
+        </div>
         </div>
       </div>
     </div>
--- src/app/design/grid/page.tsx
+++ src/app/design/grid/page.tsx
@@ -14,7 +14,7 @@ const products = [
     {
         pourcentage: 50,
         imageUrl: "/images/grid-image/menteau2.jpg",
-        name:'Manteau Classique',
+        name: 'Manteau Classique',
         oldPrice: 200,
         newPrice: 100,
         color: 'bg-amber-900'
@@ -25,7 +25,7 @@ const products = [
         name: 'Manteau Long',
         oldPrice: 180,
         newPrice: 126,
-        color:'bg-black'
+        color: 'bg-black'
     },
     {
         pourcentage: 65,
@@ -46,15 +46,15 @@ const products = [
     {
         pourcentage: 45,
         imageUrl: "/images/grid-image/jupe2.jpg",
-        name:'Jupe longue',
+        name: 'Jupe longue',
         oldPrice: 150,
-        newPrice:82,
+        newPrice: 82,
         color: 'bg-blue-500'
     },
     {
         pourcentage: 15,
         imageUrl: "/images/grid-image/robe1.jpg",
-        name:'Robe patineuse',
+        name: 'Robe patineuse',
         oldPrice: 80,
         newPrice: 68,
         color: 'bg-black'
@@ -70,9 +70,9 @@ const products = [
 ]
 export default function page() {
     return (
-        <div className='relative px-10 space-y-4'>
+        <div className='relative px-8 space-y-4'>
             {/* Header */}
-            <div className='fixed top-0 left-0 right-0 bg-white px-10 z-50 shadow-sm'>
+            <div className='fixed top-0 left-0 right-0 bg-white px-4 z-50 shadow-sm'>
                 <div className='flex justify-between items-center text-sm font-light py-2'>
                     <p>Langue:Français</p>
                     <p className='text-md font-normal'>RETOUR GRATUIT POUR TOUTE COMMANDE</p>
@@ -83,13 +83,13 @@ export default function page() {
                 </div>
                 <div className='border-1 border-gray-200' />
                 <div className='flex justify-between items-center py-4'>
-                    <h1 className='text-4xl font-bold '> marwa </h1>
+                    <h1 className='text-4xl font-bold'> marwa </h1>
                     <div className='text-2xl flex justify-between gap-14'>
                         <p>Nouveautés</p>
                         <p>E-shop</p>
                         <p>À propos</p>
                     </div>
-                    <div className='flex gap-4 items-center '>
+                    <div className='flex gap-4 items-center'>
                         <Search size={18} />
                         <Heart size={18} />
                         <UserRound size={18} />
@@ -98,7 +98,7 @@ export default function page() {
                 </div>
             </div>
             {/* Sou menu */}
-            <div className=''>
+            <div>
                 <p className='flex items-start px-4 mt-30'>Accueil / E-shop / Nouveautés</p>
                 <p className='text-center text-6xl uppercase mt-5'>Vêtements</p>
                 <div className='flex justify-between items-center mt-15'>
@@ -114,7 +114,7 @@ export default function page() {
                 </div>
             </div>
             {/* Grid */}
-            <div className='grid grid-cols-3 gap-4 my-10'>
+            <div className='grid grid-cols-3 gap-2 my-10 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 '>
                 {products.map((item, index) => (
                     <div key={index}>
                         <ProduitCard
